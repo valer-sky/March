e79b45188325141396a17cf1c2ae716824c51d7f
valer-sky <89993618+valer-sky@users.noreply.github.com>
2022-03-24 15:59:50 +0300

    made clock on svg

---
+++ Colledge work/script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let UI = {
+    time: document.querySelector('.time'),
+    second: document.querySelector('.hand--second'),
+    minute: document.querySelector('.hand--minute'),
+    hour: document.querySelector('.hand--hour'),
+};
+
+function updateClock(){
+    // GETTING TIME
+    let now = new Date();
+    let seconds = (now.getSeconds() + now.getMilliseconds() / 1000) / 60 * 360;
+    let minutes = (now.getMinutes() + now.getSeconds() / 60) / 60 * 360;
+    let hours = (now.getHours() + now.getMinutes() / 60) / 12 * 360;
+    let time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+    // UI Update
+    UI.time.textContent = time;
+    UI.second.style.transform = `rotate(${seconds}deg)`;
+    UI.minute.style.transform = `rotate(${minutes}deg)`;
+    UI.hour.style.transform = `rotate(${hours}deg)`;
+    requestAnimationFrame(updateClock);
+}
+    requestAnimationFrame(updateClock);
